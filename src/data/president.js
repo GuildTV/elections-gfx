e945d9aa22fb933d5782a1ccc1ff18acd3e91148
Robert Sumner <robfsumner@gmail.com>
2015-02-02 19:48:07 +0000

    Moved to js files for data.

---
+++ src/data/president.js
@@ -0,0 +1,24 @@
+Data['president'] = [
+  {
+    "uid"             : "pres-john",
+    "name"            : "John Doe",
+    "role"            : "President",
+    "img"             : "public/img/pres/john-doe",
+    "manifestoPoints" : {
+      "one"           : "First Manifesto Point",
+      "two"           : "Second Manifesto Point",
+      "three"         : "Third Manifesto Point",
+    }
+  },
+  {
+    "uid"             : "pres-jane",
+    "name"            : "Jane Doe",
+    "role"            : "President",
+    "img"             : "public/img/pres/jane-doe.png",
+    "manifestoPoints" : {
+      "one"           : "First Manifesto Point",
+      "two"           : "Second Manifesto Point",
+      "three"         : "Third Manifesto Point",
+    }
+  }
+];
