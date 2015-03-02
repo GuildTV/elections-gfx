1c0fd291138a51f8ec87eaa71dd1d9bdf822a2f0
Robert Sumner <robfsumner@gmail.com>
2015-03-02 16:29:29 +0000

    not sure

--- public/js/data.js
+++ public/js/data.js
@@ -1,43 +1,4 @@
 var Data = [];
-Data['president'] = [
-  {
-    "uid"             : "pres-jack",
-    "pid"             : "pres",
-    "name"            : "Jack 'is Back' Mably",
-    "position"        : "President",
-    "img"             : "/elections-2015/public/img/roles/pres/jack.png",
-    "manifestoPoints" : {
-      "one"           : "Bigger, Better & Varied On Campus Events",
-      "two"           : "Helping You Grab a Grad Job",
-      "three"         : "Making Campus Work for You",
-    }
-  },
-  {
-    "uid"             : "pres-ed",
-    "pid"             : "pres",
-    "name"            : "Edward Sainsbury",
-    "position"        : "President",
-    "img"             : "/elections-2015/public/img/roles/pres/ed.png",
-    "manifestoPoints" : {
-      "one"           : "Extend 24 Hour Library",
-      "two"           : "Secure Sponsorship For More Campus Events",
-      "three"         : "Stronger Ties with Alumni",
-    }
-  },  {
-    "uid"             : "pres-suleiman",
-    "pid"             : "pres",
-    "name"            : "Suleiman Saidu Suleiman",
-    "position"        : "President",
-    "img"             : "/elections-2015/public/img/roles/pres/suleiman.png",
-    "manifestoPoints" : {
-      "one"           : "To provide a more conducive learning environment for all students",
-      "two"           : "To provide more learning support especially for the international students.",
-      "three"         : "To ensure equal representation for all students.",
-    }
-  },
-
-];
-
 Data['sabbs'] = [
   {
     "uid"             : "sabbs-pres",
@@ -92,33 +53,6 @@ Data['talent'] = [
     "name"            : "Joe McDowell & XX",
     "position"        : "Analysts",
   },
-];
-
-Data['vpad'] = [
-  {
-    "uid"             : "pres-john",
-    "pid"             : "vpad",
-    "name"            : "John Doe",
-    "position"        : "Vice President (Activities and Development)",
-    "img"             : "http://lorempixel.com/200/200/people",
-    "manifestoPoints" : {
-      "one"           : "First Manifesto Point",
-      "two"           : "Second Manifesto Point",
-      "three"         : "Third Manifesto Point",
-    }
-  },
-  {
-    "uid"             : "pres-jane",
-    "pid"             : "vpad",
-    "name"            : "Jane Doe",
-    "position"        : "Vice President (Activities and Development)",
-    "img"             : "http://lorempixel.com/200/200/people",
-    "manifestoPoints" : {
-      "one"           : "First Manifesto Point",
-      "two"           : "Second Manifesto Point",
-      "three"         : "Third Manifesto Point",
-    }
-  }
 ];
 
 Data['president'] = [
