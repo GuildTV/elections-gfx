182a976347d469e088cd161433eb22ce20e84a73
Robert Sumner <robfsumner@gmail.com>
2015-03-02 16:28:47 +0000

    [elections-2015] data changes

--- public/js/data.js
+++ public/js/data.js
@@ -38,6 +38,39 @@ Data['president'] = [
 
 ];
 
+Data['sabbs'] = [
+  {
+    "uid"             : "sabbs-pres",
+    "name"            : "Poppy Wilkinson",
+    "position"        : "President",
+  },
+  {
+    "uid"             : "sabbs-vpad",
+    "name"            : "Wadim Wesolek",
+    "position"        : "Vice-President (Activities & Development)",
+  },
+  {
+    "uid"             : "sabbs-vpdrs",
+    "name"            : "Bethan Dovey",
+    "position"        : "Vice-President (Democracy, Resources & Sustainability)",
+  },
+  {
+    "uid"             : "sabbs-vpe",
+    "name"            : "Joe Armer",
+    "position"        : "Vice-President (Education)",
+  },
+  {
+    "uid"             : "sabbs-vps",
+    "name"            : "Molly Browne",
+    "position"        : "Vice-President (Sport)",
+  },
+  {
+    "uid"             : "sabbs-vpw",
+    "name"            : "Frankie Greenwell",
+    "position"        : "Vice-President (Welfare)",
+  },
+];
+
 Data['talent'] = [
   {
     "uid"             : "talent-update",
@@ -59,6 +92,72 @@ Data['talent'] = [
     "name"            : "Joe McDowell & XX",
     "position"        : "Analysts",
   },
+];
+
+Data['vpad'] = [
+  {
+    "uid"             : "pres-john",
+    "pid"             : "vpad",
+    "name"            : "John Doe",
+    "position"        : "Vice President (Activities and Development)",
+    "img"             : "http://lorempixel.com/200/200/people",
+    "manifestoPoints" : {
+      "one"           : "First Manifesto Point",
+      "two"           : "Second Manifesto Point",
+      "three"         : "Third Manifesto Point",
+    }
+  },
+  {
+    "uid"             : "pres-jane",
+    "pid"             : "vpad",
+    "name"            : "Jane Doe",
+    "position"        : "Vice President (Activities and Development)",
+    "img"             : "http://lorempixel.com/200/200/people",
+    "manifestoPoints" : {
+      "one"           : "First Manifesto Point",
+      "two"           : "Second Manifesto Point",
+      "three"         : "Third Manifesto Point",
+    }
+  }
+];
+
+Data['president'] = [
+  {
+    "uid"             : "pres-jack",
+    "pid"             : "pres",
+    "name"            : "Jack 'is Back' Mably",
+    "position"        : "President",
+    "img"             : "public/img/roles/pres/jack.png",
+    "manifestoPoints" : {
+      "one"           : "Bigger, Better & Varied On Campus Events",
+      "two"           : "Helping You Grab a Grad Job",
+      "three"         : "Making Campus Work for You",
+    }
+  },
+  {
+    "uid"             : "pres-ed",
+    "pid"             : "pres",
+    "name"            : "Edward Sainsbury",
+    "position"        : "President",
+    "img"             : "public/img/roles/pres/ed.png",
+    "manifestoPoints" : {
+      "one"           : "Extend 24 Hour Library",
+      "two"           : "Secure Sponsorship For More Campus Events",
+      "three"         : "Stronger Ties with Alumni",
+    }
+  },  {
+    "uid"             : "pres-suleiman",
+    "pid"             : "pres",
+    "name"            : "Suleiman Saidu Suleiman",
+    "position"        : "President",
+    "img"             : "public/img/roles/pres/suleiman.png",
+    "manifestoPoints" : {
+      "one"           : "To provide a more conducive learning environment for all students",
+      "two"           : "To provide more learning support especially for the international students.",
+      "three"         : "To ensure equal representation for all students.",
+    }
+  },
+
 ];
 
 Data['vpad'] = [
---
+++ src/data/sabbs.js
@@ -0,0 +1,32 @@
+Data['sabbs'] = [
+  {
+    "uid"             : "sabbs-pres",
+    "name"            : "Poppy Wilkinson",
+    "position"        : "President",
+  },
+  {
+    "uid"             : "sabbs-vpad",
+    "name"            : "Wadim Wesolek",
+    "position"        : "Vice-President (Activities & Development)",
+  },
+  {
+    "uid"             : "sabbs-vpdrs",
+    "name"            : "Bethan Dovey",
+    "position"        : "Vice-President (Democracy, Resources & Sustainability)",
+  },
+  {
+    "uid"             : "sabbs-vpe",
+    "name"            : "Joe Armer",
+    "position"        : "Vice-President (Education)",
+  },
+  {
+    "uid"             : "sabbs-vps",
+    "name"            : "Molly Browne",
+    "position"        : "Vice-President (Sport)",
+  },
+  {
+    "uid"             : "sabbs-vpw",
+    "name"            : "Frankie Greenwell",
+    "position"        : "Vice-President (Welfare)",
+  },
+];
