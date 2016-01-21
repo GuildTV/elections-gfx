ca53b848220913c5906b2ea6a923f7f2af3e39d7
Julian Waller <git@julusian.co.uk>
2016-01-21 22:00:48 +0000

    Tweaks

--- public/simple-js/winning-candidate.js
+++ public/simple-js/winning-candidate.js
@@ -35,8 +35,9 @@ function animate(){
   var blurData = { myVal: 10 };
 
   tl.to(imageWrapper, 0.6, { ease: Power1.easeOut, css: { scale:1 } })
-    .to(imageText, 0.4, { ease: Power2.easeOut, top: 0 }, '-=0.44')
-    .to(dataWrapper, 0.28, { ease: Power2.easeOut, css: { x: 0 } }, 0.64)
+    .to(imageText, 0.4, { ease: Power2.easeOut, top: 0 }, '-=0.44');
+  
+  tl.to(dataWrapper, 0.36, { ease: Power1.easeOut, css: { x: 0 } }, 0.72)
     .to(blurData, 0.28, { ease: Power2.easeIn, myVal: 0, onUpdate: animateBlur, onUpdateParams: ["{self}", dataWrapper, blurData] }, '-=0.28');
 }
 
